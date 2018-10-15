44c37fbfe1be8814e47f829679f215a54000563f
severalpun <newrey9227@gmail.com>
2018-10-15 21:27:30 +0700

    set port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const port = process.env.PORT || 4000
 
 const app = express()
 
@@ -13,5 +14,7 @@ require('./server/routes')(app)
 app.all('*', (req, res) => res.status(200).send({
     message: 'Welcome to the beginning of nothingness.',
 }));
+
+app.listen(port, () => console.log('App Deploy At Port ' , port , ' !!'))
 
 module.exports = app
